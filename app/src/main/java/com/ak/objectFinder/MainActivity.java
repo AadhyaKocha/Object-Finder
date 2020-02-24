d3be982ed9870c8725f9e19228ef53b5cb52461a
zhangjoe99 <zhangjoe99>
2020-02-24 12:33:15 -0500

    Added some user authentication commented out

--- app/src/main/java/com/ak/objectFinder/MainActivity.java
+++ app/src/main/java/com/ak/objectFinder/MainActivity.java
@@ -1,18 +1,35 @@
 package com.ak.objectFinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    public FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mAuth = FirebaseAuth.getInstance();
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//
+//        //updateUI(currentUser);
+
         ToggleButton audioToggle = (ToggleButton)findViewById(R.id.audioBtn);
         ToggleButton notifyToggle = (ToggleButton)findViewById(R.id.notifyBtn);
     }
@@ -32,4 +49,36 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+//    public void createAccount(String email, String password) {
+//        mAuth.createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            mUser = mAuth.getCurrentUser();
+//                            //updateUI(user);
+//                        } else {
+//                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+//                            //updateUI(null);
+//                        }
+//                    }
+//                });
+//    }
+//
+//    public void signIn(String email, String password) {
+//        mAuth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            mUser = mAuth.getCurrentUser();
+//                            //updateUI(user);
+//                        } else {
+//                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+//                            //updateUI(null);
+//                        }
+//                    }
+//                });
+//    }
 }
