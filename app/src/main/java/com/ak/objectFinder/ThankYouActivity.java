e0f496fddc231abdd9ec7816f879d83aae9afec1
AadhyaKocha <51734801+AadhyaKocha@users.noreply.github.com>
2020-02-26 18:51:57 -0500

    ThankYou activity added

---
+++ app/src/main/java/com/ak/objectFinder/ThankYouActivity.java
@@ -0,0 +1,14 @@
+package com.ak.objectFinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThankYouActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thank_you);
+    }
+}
