82ee2e5a3ef499029c4426113be371dae3078ce8
AadhyaKocha <51734801+AadhyaKocha@users.noreply.github.com>
2020-02-28 08:42:01 -0500

    Vidyo.io set up

--- app/src/main/java/com/ak/objectFinder/VideoCall.java
+++ app/src/main/java/com/ak/objectFinder/VideoCall.java
@@ -2,15 +2,34 @@ package com.ak.objectFinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-//import com.vidyo.VidyoClient.Connector.VidyoConnector;
-//import com.vidyo.VidyoClient.Connector.Connector;
+import android.view.View;
+import android.widget.FrameLayout;
 
-public class VideoCall extends AppCompatActivity {
+import com.vidyo.VidyoClient.Connector.ConnectorPkg;
+//import com.vidyo.VidyoClient.Connector.VidyoConnector;
+import com.vidyo.VidyoClient.Connector.Connector;
+
+public class VideoCall extends AppCompatActivity{
 
 //    private VidyoConnector vidyoConnector;
+    private FrameLayout videoFrame;
+    private Connector mVidyoConnector = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videocall);
+
+        ConnectorPkg.setApplicationUIContext(this);
+        ConnectorPkg.initialize();
+        videoFrame = (FrameLayout)findViewById(R.id.video_frame);
+    }
+
+    public void Start(View view) {
+        mVidyoConnector = new Connector(videoFrame, Connector.ConnectorViewStyle.VIDYO_CONNECTORVIEWSTYLE_Default,
+                7,
+                "",
+                "",
+                0);
+        mVidyoConnector.showViewAt(videoFrame, 0, 0, videoFrame.getWidth(), videoFrame.getHeight());
     }
 }
